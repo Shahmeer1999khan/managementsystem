1676363c8cc8fbbf2ebd53db20fa666d9557305d
Shahmeer1999khan <shahmeer1999khan@gmail.com>
2023-12-13 18:29:08 +0500

    changes

--- employee_management/tasks.py
+++ employee_management/tasks.py
@@ -1,20 +1,12 @@
 from celery import shared_task
-from django.shortcuts import get_object_or_404
-from .models import Employee
-from time import sleep
-from django.core.exceptions import ObjectDoesNotExist
-# def delete_employee_task(employee_id):
-#     print('ssssssssssssssssssssssssssss')
-#     try:
-#         employee = Employee.objects.get(id=employee_id)
-#         employee.delete()
-#         return f"Employee {employee_id} deleted successfully."
-#     except ObjectDoesNotExist:
-#         return f"Employee {employee_id} does not exist."
 
-@shared_task(bind=True)
-def test_func(self):
-    print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
-    for i in range(10):
-        print(i)
-    return "DONE"
+@shared_task
+def test_func():
+    try:
+        for i in range(10):
+            print(i)
+        print("Task executed successfully!")
+        return {'status': 'Task executed successfully!'}
+    except Exception as e:
+        print(f"Task execution failed: {e}")
+        return {'status': 'Task execution failed'}
